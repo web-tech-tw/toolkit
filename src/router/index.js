90355a4592882da352e82313eb7ea14f3d035532
不知火 Shiranui <supersonic@livemail.tw>
2024-11-13 14:23:26 +0800

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    component: () => import("../views/HomeView.vue"),
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    component: () => import("../views/NotFoundView.vue"),
+  },
+];
+
+const router = createRouter({
+  scrollBehavior: () => ({top: 0}),
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
